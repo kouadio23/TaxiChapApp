29d8e4c0ca16829a7845adc0805be7dce6d94d31
Kouadio Kouao <kouaokouadio@gmail.com>
2024-03-28 14:25:13 -0600

    reservations/views.py

--- taxi_chap_backend/reservations/views.py
+++ taxi_chap_backend/reservations/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Reservation, Vehicle
+from django.http import JsonResponse
+
+# Vue pour lister toutes les réservations
+class ReservationList(View):
+    def get(self, request, *args, **kwargs):
+        reservations = Reservation.objects.all()
+        data = {"reservations": list(reservations.values("passenger", "vehicle", "start_location", "end_location", "status"))}
+        return JsonResponse(data)
+
+# Vue pour créer une nouvelle réservation
+class ReservationCreate(View):
+    def post(self, request, *args, **kwargs):
+        # Ici, vous traiteriez les données POST pour créer une nouvelle réservation.
+        # Cette partie est simplifiée pour l'exemple.
+        return JsonResponse({"message": "Nouvelle réservation créée"}, status=201)
+
 
 # Create your views here.
